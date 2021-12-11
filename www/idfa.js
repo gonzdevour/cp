aef5b9dda86d868991b01752db3a890a591b24f6
gonzdevour <jyunru.li@gmail.com>
2021-12-12 02:02:00 +0800

    a

--- www/idfa.js
+++ www/idfa.js
@@ -1,19 +1,22 @@
-cordova.plugins.idfa.getInfo(onSuccess)
-    .then(info => {
-        if (!info.trackingLimited) {
-            return info.idfa || info.aaid;
-        } else if (info.trackingPermission === idfaPlugin.TRACKING_PERMISSION_NOT_DETERMINED) {
-            return idfaPlugin.requestPermission().then(result => {
-                if (result === idfaPlugin.TRACKING_PERMISSION_AUTHORIZED) {
-                    return idfaPlugin.getInfo().then(info => {
-                        return info.idfa || info.aaid;
-                    });
-                }
+var cdv_idfa_getinfo = function (onSuccess) {
+  cordova.plugins.idfa
+    .getInfo()
+    .then((info) => {
+      if (!info.trackingLimited) {
+        return info.idfa || info.aaid;
+      } else if (info.trackingPermission === idfaPlugin.TRACKING_PERMISSION_NOT_DETERMINED) {
+        return idfaPlugin.requestPermission().then((result) => {
+          if (result === idfaPlugin.TRACKING_PERMISSION_AUTHORIZED) {
+            return idfaPlugin.getInfo().then((info) => {
+              return info.idfa || info.aaid;
             });
-        }
+          }
+        });
+      }
     })
-    .then(idfaOrAaid => {
-        if (idfaOrAaid) {
-          onSuccess(idfaOrAaid);
-        }
+    .then((idfaOrAaid) => {
+      if (idfaOrAaid) {
+        onSuccess(idfaOrAaid);
+      }
     });
+};
